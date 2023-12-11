aa59fc9b858cb3c0609e1871ad58b8f378a33900
AravisVLenfest <glenfest@gmail.com>
2023-12-11 01:43:19 +0000

    pull request

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@ int main() {
   std::cout << "Hello World!\n";
   int a = 0;
   std::cin >> a;
-  std::cout << a-2;
+  std::cout << a - 2;
+  std::cout << ":)";
+  for (int i = 1; i < 11; i++) {
+    std::cout << i << " duck" << std::endl;
+  }
 }
